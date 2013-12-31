833a268cadccea3b6637f46f9630314ca959ba23
Fan-Yu Kuan <fykuan@gmail.com>
2013-12-31 14:06:26 +0800

    first working commit

--- README.md
+++ README.md
@@ -2,3 +2,36 @@ dropbox_camera_uploads_fetch
 ============================
 
 把Dropbox上的Camera Uploads資料夾下載並刪除
+
+### 準備
+
+1. 到 [https://www.dropbox.com/developers/core/sdks/python](https://www.dropbox.com/developers/core/sdks/python)下載 Dropbox Python SDK
+
+2. 解壓縮並安裝
+	
+	python setup.py install
+
+3. 到 [https://www.dropbox.com/developers/apps](https://www.dropbox.com/developers/apps) 選「Create APP Key」
+4. 選擇「Dropbox API app」
+5. 選擇「Files and datastores」
+6. 選擇「No」
+7. 選擇「All file types」
+8. 輸入App name
+9. 將產生的App key和App secret依以下格式建立apikey.json
+
+		{	
+			"key": "kkkkkkkkkkkkk",
+ 		    "secret": "sssssssssssss"
+		}
+
+### 執行
+
+1. 執行dropbox_camera_uploads_bak.py [儲存目錄]，例：
+
+		dropbox_camera_uploads_bak.py /tmp
+		
+2.	初次執行要到程式提示的網誌確認後取得authorization code並輸入進程式中
+
+### 遇到問題
+
+遇到問題請開issue囉
---
+++ dropbox_camera_uploads_bak.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import dropbox
+import json
+import re
+import sys
+
+
+apikey = ''
+apisecret = ''
+accesstoken=''
+path = ''
+
+if len(sys.argv) != 2:
+    print "Please specify file saving directory"
+    sys.exit(0)
+else:
+    path = sys.argv[1]
+
+try:
+    with open('.accesstoken'):
+        f_accesstoken = open('.accesstoken', 'r')
+        accesstoken = f_accesstoken.read()
+except IOError:
+    print ".accesstoken not found"
+    try:
+        with open('apikey.json'):
+            f_apikey = open('apikey.json', 'r');
+            json_apikey = json.loads(f_apikey.read())
+            flow = dropbox.client.DropboxOAuth2FlowNoRedirect(json_apikey['key'], json_apikey['secret'])
+            authorize_url = flow.start()
+            print '1. Go to: ' + authorize_url
+            print '2. Click "Allow" (you might have to log in first)'
+            print '3. Copy the authorization code.'
+            code = raw_input("Enter the authorization code here: ").strip()
+            accesstoken, user_id = flow.finish(code)
+            # Write accesstoken to .accesstoken
+            f_accesstoken = open ('.accesstoken', 'a+')
+            f_accesstoken.write(accesstoken)
+    except IOError:
+        print "apikey.json not found"
+        sys.exit(0)
+
+
+client = dropbox.client.DropboxClient(accesstoken)
+
+# 取得/Camera Uploads資料夾內容
+folder_metadata = client.metadata('/Camera Uploads')
+
+for i in range(0,len(folder_metadata['contents'])):
+    abs_filepath = folder_metadata['contents'][i]['path']
+    filename = re.match("^.*/(.*)$", abs_filepath).group(1)
+    ofile = open(path+"/"+filename, "w+")
+    f, metadata = client.get_file_and_metadata(abs_filepath)
+
+    # 寫入檔案
+    ofile.write(f.read())
+    ofile.close()
+    print "dropbox:" + abs_filepath + " has downloaded to " + path + "/" + filename
+
+    # 刪除Dropbox上的檔案
+    client.file_delete(abs_filepath)
